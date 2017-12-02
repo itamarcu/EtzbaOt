b2f25f43a30f0d581ff1a751d788b124a28ca9e1
Daniel Kiszkiel <dkiszkiel@9bits.pl>
2017-12-02 16:56:02 +0100

    Sample keyboard project

--- app/src/main/java/com/example/cukier/shemboard/ShemboardService.java
+++ app/src/main/java/com/example/cukier/shemboard/ShemboardService.java
@@ -7,46 +7,38 @@ import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
-public class ShemboardService extends InputMethodService
-{
-    public ShemboardService()
-    {
+public class ShemboardService extends InputMethodService {
+    public ShemboardService() {
     }
-    
+
     LinearLayout inputView;
     ArrayList<Button> buttons = new ArrayList<>();
-    
-    @Override
-    public View onCreateInputView()
-    {
+
+    @Override public View onCreateInputView() {
         inputView = (LinearLayout) getLayoutInflater().inflate(
                 R.layout.input, null);
         this.setupLayout();
         return inputView;
     }
-    
-    void setupLayout()
-    {
+
+    void setupLayout() {
         buttons.add((Button) inputView.findViewById(R.id.button1));
         buttons.add((Button) inputView.findViewById(R.id.button2));
         buttons.add((Button) inputView.findViewById(R.id.button3));
         buttons.add((Button) inputView.findViewById(R.id.button4));
         buttons.add((Button) inputView.findViewById(R.id.button5));
-        
-        for (int i = 0; i < buttons.size(); i++)
-        {
+
+        for (int i = 0; i<buttons.size(); i++) {
             final int ii = i;
             Button b = this.buttons.get(ii);
-            b.setOnClickListener(new View.OnClickListener()
-            {
+            b.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view)
-                {
+                public void onClick(View view) {
                     getCurrentInputConnection().commitText(Integer.toString(ii), 1);
                 }
             });
         }
     }
-    
-    
+
+
 }
