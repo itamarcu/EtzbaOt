02506e7edec928e8c272b842afd40c0e6d46597f
Itamar Curiel <itamarcu@gmail.com>
2017-12-10 19:47:21 +0200

    Initial Commit - nothing works right now, just draws points

---
+++ app/src/main/java/com/example/cukier/shemboard/KeyMap.java
@@ -0,0 +1,93 @@
+package com.example.cukier.shemboard;
+
+public class KeyMap
+{
+    @SuppressWarnings("SpellCheckingInspection")
+    public enum Map
+    {
+        /**
+         * Alphabetical order for the binary numbers.
+         */
+        ABDHP()
+                {
+                    @Override
+                    String code()
+                    {
+                        return "abcd" +
+                                "efgh" +
+                                "ijkl" +
+                                "mnop" +
+                                "qrst" +
+                                "uvwx" +
+                                "yz#-" +
+                                "#$ "
+                                ;
+                    }
+                },
+        /**
+         * Frequency order for the binary numbers.
+         */
+        FREQUENCY_ORDER()
+                {
+                    @Override
+                    String code()
+                    {
+                        return "etao" +
+                                "insr" +
+                                "hldc" +
+                                "umfp" +
+                                "gwyb" +
+                                "vkxj" +
+                                "qz#-" +
+                                "#$ "
+                                ;
+                    }
+                },
+        /**
+         * Frequency order matched with ease of typing each number.
+         * <p>
+         * 1 touch: 1 2 4 8 16
+         * e t a o i
+         * <p>
+         * 2 touch: 3 6 12 24 5 10 20 9 18 17
+         * n s r h l d c u m f
+         * <p>
+         * 3 touch nice: 7 14 28 21
+         * p g [del] w
+         * <p>
+         * 3 touch ok: 13 26 25 19 11
+         * y b v k x
+         * <p>
+         * 3 touch bad: 22
+         * [none]
+         * <p>
+         * 4 touch: 15 23 27 29 30
+         * j [none] q z [special]
+         */
+        ETAOI()
+                {
+                    @Override
+                    String code()
+                    {
+                        return "etna" +
+                                "lspo" +
+                                "udxr" +
+                                "ygji" +
+                                "fmkc" +
+                                "w##h" +
+                                "vbq-" +
+                                "z$ "
+                                ;
+                    }
+                };
+        
+        abstract String code();
+        
+        public char decode(int index)
+        {
+            if (index - 1 < 32)
+                return code().charAt(index - 1);
+            return '%'; //bug
+        }
+    }
+}
--- app/src/main/java/com/example/cukier/shemboard/ShemboardService.java
+++ app/src/main/java/com/example/cukier/shemboard/ShemboardService.java
@@ -1,44 +1,150 @@
 package com.example.cukier.shemboard;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.inputmethodservice.InputMethodService;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ShemboardService extends InputMethodService {
-    public ShemboardService() {
+public class ShemboardService extends InputMethodService
+{
+    private KeyMap.Map map;
+    private LinearLayout inputView;
+    private boolean[] currentlyPressedButtons = new boolean[5];
+    private boolean[] previouslyPressedButtons = new boolean[5];
+    private int specialMode;
+    private ImageView imageView6;
+    private Canvas canvas6;
+    private Bitmap bitmap6;
+    private List<Point> fingerPoints;
+    
+    public ShemboardService()
+    {
+        map = KeyMap.Map.ABDHP;
+        resetButtonPresses();
+        fingerPoints = new ArrayList<>();
     }
-
-    LinearLayout inputView;
-    ArrayList<Button> buttons = new ArrayList<>();
-
-    @Override public View onCreateInputView() {
+    
+    private void resetButtonPresses()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            currentlyPressedButtons[i] = false;
+            previouslyPressedButtons[i] = false;
+        }
+        
+        specialMode = 0;
+    }
+    
+    @Override
+    public View onCreateInputView()
+    {
         inputView = (LinearLayout) getLayoutInflater().inflate(
                 R.layout.input, null);
         this.setupLayout();
         return inputView;
     }
-
-    void setupLayout() {
-        buttons.add((Button) inputView.findViewById(R.id.button1));
-        buttons.add((Button) inputView.findViewById(R.id.button2));
-        buttons.add((Button) inputView.findViewById(R.id.button3));
-        buttons.add((Button) inputView.findViewById(R.id.button4));
-        buttons.add((Button) inputView.findViewById(R.id.button5));
-
-        for (int i = 0; i<buttons.size(); i++) {
-            final int ii = i;
-            Button b = this.buttons.get(ii);
-            b.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    getCurrentInputConnection().commitText(Integer.toString(ii), 1);
-                }
-            });
-        }
+    
+    @Override
+    public void onStartInputView(EditorInfo info, boolean restarting)
+    {
+        super.onStartInputView(info, restarting);
+        
+        
+        //Set size to maximum screen
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        
+        ViewGroup.LayoutParams params = inputView.getLayoutParams();
+        params.height = height;
+        inputView.setLayoutParams(params);
+        
+        //Image view
+        imageView6 = inputView.findViewById(R.id.imageView6);
+        //create a new canvas with a transparent background
+        bitmap6 = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
+        canvas6 = new Canvas(bitmap6);
+        updateCanvas();
     }
-
-
+    
+    void setupLayout()
+    {
+        inputView.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                return ShemboardService.this.onTouch(view, motionEvent);
+            }
+        });
+        
+    }
+    
+    public boolean onTouch(View view, MotionEvent motionEvent)
+    {
+        Point touchPlace = new Point();
+        touchPlace.set((int) motionEvent.getX(), (int) motionEvent.getY());
+        Paint pointPaint = new Paint();
+        pointPaint.setARGB(128, 60, 200, 0);
+        canvas6.drawCircle(touchPlace.x, touchPlace.y, 10, pointPaint);
+        updateCanvas();
+        return false;
+    }
+    
+    private void updateCanvas()
+    {
+        imageView6.setImageBitmap(bitmap6);
+    }
+    
+    private void onAllButtonsReleased()
+    {
+        
+        char character = '&';
+        int binary = 0;
+        for (int i = 0; i < 5; i++)
+            if (previouslyPressedButtons[i])
+                binary += 1 << i; // 2 to the power of i
+        
+        
+        if (binary >= 32)
+            Log.d("Binary", "ERROR: binary number too large: " + binary);
+        else
+            character = map.decode(binary);
+        
+        if (specialMode == 0) //no special char
+        {
+            if (character == '#') //None
+            {
+                getCurrentInputConnection().commitText("#", 1);
+            }
+            else if (character == '-') //Backspace
+            {
+                getCurrentInputConnection().deleteSurroundingText(-1, 0);
+            }
+            else if (character == '$') //Special
+            {
+            
+            }
+            else //Normal character
+            {
+                getCurrentInputConnection().commitText("" + character, 1);
+            }
+        }
+        
+        resetButtonPresses();
+    }
+    
 }
